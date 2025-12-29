54e0ff39734cd7f6a911cdbba8d98080df079c8b
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-12-29 19:23:31 -0300

    python pandas series  29/12/2025

---
+++ pandas/criando_series.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+lista1=['a', 'b', 'c']
+lista2=[1, 2, 3]
+arr = np.array([10, 20, 30])
+d = {'a': 100,'b': 200, 'c': 300}
+serie = pd.Series(data = lista2, index = lista1)
+print(serie)
+print(serie['a'])
+serie = pd.Series(lista2, lista1)
+print(f'a serie é \n{serie}') 
+serie1 = pd.Series(arr, lista1)
+print(f"a sere1 é \n{serie1}")
+serie2 = pd.Series(arr)
+print(f"a sere2 é \n{serie2}")
+serie2 = pd.Series(d)
+print(f"a sere2 é \n{serie2}")
+
+
